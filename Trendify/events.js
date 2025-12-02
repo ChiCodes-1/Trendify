92a3372689c31988e1755a2c1bb77e3efecc07c3
ChiCodes-1 <chimereze.ibebuike@miva.edu.ng>
2025-12-03 00:32:13 +0100

    created the trendify event page and added js into it plus media queries for responsiveness

---
+++ Trendify/events.js
@@ -0,0 +1,6 @@
+document.getElementById('reminderBtn').addEventListener('click', function () {
+  const eventName = 'Trendify Product Drop';
+  const eventDate = 'December 15th, 2025';
+
+  alert('Reminder Added\n\nEvent: ' + eventName + '\nDate: ' + eventDate);
+});
